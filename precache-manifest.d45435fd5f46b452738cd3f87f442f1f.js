406665f4a521fd3b52e82b5822330d8d556e9396
dhafer touati <dhafer127@gmail.com>
2019-07-31 14:12:47 +0100

    Updates

---
+++ precache-manifest.d45435fd5f46b452738cd3f87f442f1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef563f84ba0fde3e8644a197399de29",
+    "url": "/docplannerReact/index.html"
+  },
+  {
+    "revision": "d5f56989ace6ddb7f604",
+    "url": "/docplannerReact/static/css/main.3accc808.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/docplannerReact/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "d5f56989ace6ddb7f604",
+    "url": "/docplannerReact/static/js/main.ce659958.chunk.js"
+  },
+  {
+    "revision": "6c17e8b18fde78ceacf4",
+    "url": "/docplannerReact/static/js/runtime~main.ca513ad7.js"
+  }
+]);
